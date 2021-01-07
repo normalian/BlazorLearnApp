231131c749550c7cc4a7c98117c4798611e8bcbd
Daichi Isami <daisami@microsoft.com>
2021-01-07 17:12:12 +0900

    add form validation

--- BlazorLearnApp/Models/Restaurant.cs
+++ BlazorLearnApp/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace BlazorLearnApp.Models
 {
     public class Restaurant
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "Input restaurant name")]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Input City name")]
         public string City { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 2, MinimumLength = 2, ErrorMessage = "Input State Name")]
         public string State { get; set; }
     }
 }
